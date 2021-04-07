0784bab3a5ff5fc5bbb075957ca25d4bfb4146da
hozaifa <hozaifa.mib@gmail.com>
2021-04-07 16:46:02 +0200

    Add project files.

---
+++ Infrastructure/ERPContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ERPContext : IdentityDbContext
+    {
+        public ERPContext(DbContextOptions<ERPContext> options):base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
